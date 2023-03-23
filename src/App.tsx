d3e35ddd11f3103a705f5671c5a336e1ceb0ec40
Yashdeep Bachhas <yashdeepbachhas93@gmail.com>
2023-03-24 03:29:31 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,7 @@ import Calendar from './components/Calendar';
 
 function App() {
   return (
-    <>
-      <Calendar date='23/03/2020' />
-      <h2>TODO</h2>
-      <ul>
-        <li>update height of calendar to remove extra blank space from bottom</li>
-        <li>try grid</li>
-        <li>add test cases</li>
-      </ul>
-    </>
+    <Calendar date='23/03/2020' />
   );
 }
 
